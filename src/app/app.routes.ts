58e82213488906dbce24d98a629695f5ca84a364
Lautaro <pperalta.lautaro@gmail.com>
2025-09-17 10:04:05 -0300

    Tercera actualizacion Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,24 @@ import { ContactsPage } from './pages/contacts-page/contacts-page';
 import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
 import { LoggedLayout } from './layouts/logged-layout/logged-layout';
 import { RegisterPage } from './pages/register-page/register-page';
+import { onlyPublicUserGuard } from './guards/only-public-user-guard';
+import { onlyLoggedUserGuard } from './guards/only-logged-user-guard';
 
 export const routes: Routes = [
     {
         path: 'login',
-        component: LoginPage
+        component: LoginPage,
+        canActivate: [onlyPublicUserGuard]
     },
     {
         path: 'register',
-        component: RegisterPage
+        component: RegisterPage,
+        canActivate: [onlyPublicUserGuard]
     },
     {
         path: '',
         component: LoggedLayout,
+        canActivateChild: [onlyLoggedUserGuard],
         children: [
             {
                 path: '',
